98cd005d86bac1d98b424330ff438301bd01a492
99kies <1290017556@qq.com>
2019-12-06 20:54:43 +0800

    main function

--- py2to3.py
+++ py2to3.py
@@ -7,7 +7,7 @@ if WIN:
 else:
     path_sprit = '/'
 
-def tochange(path):
+def py2to3(path):
     '''
     :param path: Absolute path to the target project folder
     :return: no return
@@ -15,16 +15,15 @@ def tochange(path):
     for i in os.listdir(path):
         path2 = os.path.join(path, i)
         if os.path.isdir(path2): # 如果为文件夹，则进入递归
-            tochange(path2)
+            py2to3(path2)
         else:
-            if 'py' in i and len(i.split('.')) == 2: # 如果不为文件夹并且为python文件则进行转换
+            if 'py' == i.split('.')[1] and len(i.split('.')) == 2: # 如果不为文件夹并且为python文件则进行转换
                 dir = path+path_sprit+i
                 print(dir)
-                print((os.system(r'python 2to3.py -w %s' % dir)))
+                print(os.system(r'python 2to3.py -w %s' % dir))
                 # Make the conversion if you through: pip install 2to3 .can use: os.system(r'2to3 -w %s' % dir）
 
 
-# tochange(path)
 if __name__ == '__main__':
     print('Please provide an absolute path parameter.')
     try:
@@ -32,5 +31,5 @@ if __name__ == '__main__':
         print(("Let's look in %s." % path))
     except:
         print("Error, You Need Offer A Filepath!!! ")
-    tochange(path)
+    py2to3(path)
 
